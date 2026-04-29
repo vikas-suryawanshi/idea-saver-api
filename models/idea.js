e2d40023d51174702fc87a3d4ea845b34e6e6d9d
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-04-29 15:41:40 +0530

    define schema

---
+++ models/idea.js
@@ -0,0 +1,37 @@
+const mongoose=require("mongoose");
+const { type } = require("node:os");
+
+const ideaSchema=new mongoose.Schema({
+    author:{
+        type:String,
+        required:true,
+    },
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+    likes:{
+        type:Number,
+        default:0,
+    },
+    comments:[{
+        text:{
+            type:String,
+            required:true,
+        },
+        author:{
+            type:String,
+        }    
+    }],
+});
+
+const idea= mongoose.model("idea",ideaSchema);
+module.exports=idea;
