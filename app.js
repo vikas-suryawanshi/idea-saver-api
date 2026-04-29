605114348c41975b263b186686ace392fafb82c0
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-04-29 15:54:13 +0530

    mistake correct

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port=8080;
 const app=express();
 const mongoose=require("mongoose");
 const { create } = require("./models/idea");
-const Idea=reuire("./models/idea.js");
+const Idea=require("./models/idea.js");
 
 
 
@@ -19,20 +19,20 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/ideas');
 }
 
-// initalize one 
-idea1=new Idea({
-    author:"vikas",
-    title:"ai interview platform",
-    description:"it is require to indian students",
-    likes:80,
+// // initalize one 
+// idea1=new Idea({
+//     author:"vikas",
+//     title:"ai interview platform",
+//     description:"it is require to indian students",
+//     likes:80,
 
-});
+// });
 
-idea1.save().then((data)=>{
-    console.log(data);
-}).catch((err)=>{
-    console.log(err);
-})
+// idea1.save().then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// })
 
 app.get("/",(req,res)=>{
     res.send("server is working");
