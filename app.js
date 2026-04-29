caad52ad77049de923f963ee56a0b5ede3b96f2f
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-04-29 15:17:45 +0530

    connect mongoose

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express=require("express");
 const port=8080;
 const app=express();
 
+
+
+
+// mongoose connect to database
+main().then(()=>{
+    console.log("connection succesfull");
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ideas');
+}
+
 app.get("/",(req,res)=>{
     res.send("server is working");
 })
