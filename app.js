f6b48f3651b286f93c5dc01bc58eeef94c2e4af9
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-05-05 10:39:06 +0530

    deleted extra line

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const ejsMate=require("ejs-mate");
 app.engine("ejs",ejsMate);
 // reuire expresserror handling class
 const ExpressError=require("./utils/ExpressError.js");
-const ExpressError=require("./utils/wrapAsync.js");
 const wrapAsync = require("./utils/wrapAsync.js");
 
 
