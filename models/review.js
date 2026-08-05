26e2bdf6bee5c8baa5f2da0ffc07b3bfafb78653
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-05 07:48:11 +0530

    create reviewSchema

---
+++ models/review.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+    comment:String,
+    rating:{
+        type:Number,
+        min:1,
+        max:5,
+    },
+    created_At:{
+        type:Date,
+        default:Date.now(),
+
+    },
+});
