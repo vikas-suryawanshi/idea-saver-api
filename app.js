6b56784f2ef88c6fb03e18e57b83eb28536c2a8f
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-06 07:48:17 +0530

    redirect on this page

--- app.js
+++ app.js
@@ -148,6 +148,7 @@ app.post("/ideas/:id/reviews",async(req,res)=>{
     ideas.reviews.push(newReview);
     await newReview.save();
     await ideas.save();
+    res.redirect(`/ideas/${_id}`);
 })
 
 app.get("/",(req,res)=>{
