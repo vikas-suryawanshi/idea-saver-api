1f4cfc28355c7a4d4128b960f5966262f18986b8
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-07 09:14:04 +0530

    fix review Joi schema

--- schema/schema.js
+++ schema/schema.js
@@ -12,9 +12,9 @@ module.exports.updateIdeaSchema=joi.object({
     description:joi.string().required().min(3),
 });
 
-module.exports.reviewSchema = Joi.object({
-  review: Joi.object({
-    rating: Joi.number().required().min(1).max(5),
-    comment: Joi.string().required(),
+module.exports.reviewSchema = joi.object({
+  review: joi.object({
+    rating: joi.number().required().min(1).max(5),
+    comment: joi.string().required(),
   }).required(),
 });
